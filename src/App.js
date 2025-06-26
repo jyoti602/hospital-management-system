e795e61ea8cf4c04378c425344459f71ccb8e7eb
jyoti602 <jyotibisht12300@gmail.com>
2025-06-26 08:09:40 +0530

    added the api on login and contract page

--- src/App.js
+++ src/App.js
@@ -4,27 +4,13 @@ import AppRouter from './services/AppRouter';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './parts/Footer/Footer';
 
-const PUBLIC_ROUTES = [
-  '/', '/home', '/about-us', '/services', '/contact', '/feature', '/appointment', '/login'
-];
-
-function AppContent() {
-  const location = useLocation();
-  const isPublic = PUBLIC_ROUTES.includes(location.pathname);
-
-  return (
-    <>
-      {isPublic && <Navbar />}
-      <AppRouter />
-      {isPublic && <Footer/>}
-    </>
-  );
-}
 
 function App() {
   return (
     <BrowserRouter>
-      <AppContent />
+      <Navbar /> 
+      <AppRouter />
+      <Footer/>
     </BrowserRouter>
   );
 }
